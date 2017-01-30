3fb0b1076d0ab8f0ad2cecc3fb101f2b874ddf39
xHN35RQ <xHN35RQ@users.noreply.github.com>
2017-01-30 05:32:45 -0800

    merge posts and pages, tags and categories

--- assets/tipuesearch/tipuesearch_content.js
+++ assets/tipuesearch/tipuesearch_content.js
@@ -2,15 +2,31 @@
 # Content index for Tipue Search 5.0
 # https://github.com/xHN35RQ/jekyll-tipue-search
 ---
-var tipuesearch = {"pages": [
+{% assign documents = "" | split: "" %}
 {% for post in site.posts %}
-{% unless post.exclude_from_search == true %}
-	{
-		"title": {{ post.title | smartify | strip_html | normalize_whitespace | jsonify }},
-		"text": {{ post.content | strip_html | normalize_whitespace | jsonify }},
-		"tags": {% if post.tags == empty %}"",{% else %}{{ post.tags | join: ' ' | normalize_whitespace | jsonify }},{% endif %}
-		"url": {{ post.url | jsonify }}
-	}{% unless forloop.last %},{% endunless %}
-{% endunless %}
+  {% unless post.exclude_from_search == true %}
+    {% assign documents = documents | push: post %}
+  {% endunless %}
+{% endfor %}
+{% for page in site.pages %}
+  {% if page.include_in_search == true %}
+    {% assign documents = documents | push: page %}
+  {% endif %}
+{% endfor %}
+var tipuesearch = {"pages": [
+{% for document in documents %}
+  {% assign taxonomy = "" | split: "" %}
+  {% for tag in document.tags %}
+    {% assign taxonomy = taxonomy | push: tag %}
+  {% endfor %}
+  {% for category in document.categories %}
+    {% assign taxonomy = taxonomy | push: category %}
+  {% endfor %}
+  {
+    "title": {{ document.title | smartify | strip_html | normalize_whitespace | jsonify }},
+    "text": {{ document.content | strip_html | normalize_whitespace | jsonify }},
+    "tags": {% if taxonomy == empty %}"",{% else %}{{ taxonomy | join: ' ' | normalize_whitespace | jsonify }},{% endif %}
+    "url": {{ document.url | jsonify }}
+  }{% unless forloop.last %},{% endunless %}
 {% endfor %}
 ]};
